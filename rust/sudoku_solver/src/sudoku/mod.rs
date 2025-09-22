1b6cd51fabe257779f9a38d17a5d04c7f677d0cb
Undercaffinated <nathan.c.howell@proton.me>
2025-09-22 13:24:38 -0500

    Removed python from project

--- rust/sudoku_solver/src/sudoku/mod.rs
+++
@@ -1,6 +0,0 @@
-mod block;
-mod coordinates;
-mod grid_square;
-mod grid_state;
-mod solving_logic;
-pub mod sudoku;
--- rust/sudoku_solver/src/sudoku/solving_logic/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod exclusive_note_method;
-pub mod single_note_method;
-pub mod hidden_doubles;
