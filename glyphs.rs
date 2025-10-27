d21e8d50dc4d08303270c46db707541c4ee7426b
Undercaffinated <90402996+Undercaffinated@users.noreply.github.com>
2025-10-27 18:19:04 -0400

    Create Glyph enum for Sudoku digits
    Define a Glyph enum for Sudoku numbers 1-9.

---
+++ glyphs.rs
@@ -0,0 +1,20 @@
+//! # Glyphs
+//! 
+//! ## What is a Glyph?
+//! 
+//! ## Why have they been implemented?
+//! 
+//! In short, because I keep remaking an enum for the numbers 1-9 and I'm tired of it.
+
+/// The set of numbers 1-0 which can be legally written on a Sudoku board.
+pub enum Glyph {
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+}
